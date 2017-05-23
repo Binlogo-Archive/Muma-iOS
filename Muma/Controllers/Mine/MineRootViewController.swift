487cadad921f947736d7d8ed713eb6028fc5e746
Binboy <binboy@live.com>
2017-05-23 23:18:48 +0800

    clear up some useless code, build architecture, add some resources

---
+++ Muma/Controllers/Mine/MineRootViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MineRootViewController.swift
+//  Muma
+//
+//  Created by Binboy_王兴彬 on 23/05/2017.
+//  Copyright © 2017 Binboy. All rights reserved.
+//
+
+import UIKit
+
+class MineRootViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
